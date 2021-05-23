4f239b52752ce1c991abecb35494c15519390751
Andre Ravazzi <amravazzi@gmail.com>
2021-05-23 05:24:40 -0300

    feat: add a fetch example with swr hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,42 @@
 import React, { useContext, useEffect } from "react";
 import { AuthProvider, AuthContext } from "./context/AuthContext";
+import { useFetch } from "./hooks/use-fetch";
 
-function App(): JSX.Element {
+interface Auth {
+  id: string;
+  type: "login";
+  attributes: {
+    name: string;
+    email: string;
+    token: string;
+    permissions: object[];
+    relationships: [
+      {
+        type: string;
+        attributes: {
+          name: string;
+          address: string;
+          contact: string;
+          site: string;
+          type: string;
+        };
+        relationships: object[];
+      }
+    ];
+  };
+}
+
+function App() {
   const { isAuthenticated } = useContext(AuthContext);
+  const [data, error] = useFetch<Auth>("/auth/login", {
+    method: "POST",
+    payload: { username: "fulano", password: "123456" },
+  });
 
-  useEffect(() => console.log("AuthProvider", isAuthenticated), []);
+  useEffect(() => console.log("data", data), [data]);
+  useEffect(() => console.log("error", error), [error]);
 
-  return (
-    <AuthProvider>Hello world</AuthProvider>
-  );
+  return <AuthProvider>Hello world</AuthProvider>;
 }
 
 export default App;
